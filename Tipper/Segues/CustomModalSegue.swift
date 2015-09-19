55b92fc497e6885a88e6287bb04e4424f4646091
Ryan Romanchuk <rromanchuk@gmail.com>
2015-09-19 13:04:51 -0700

    Cleanup

--- Tipper/Segues/CustomModalSegue.swift
+++ Tipper/Segues/CustomModalSegue.swift
@@ -44,7 +44,7 @@ class CustomModalSegue: UIStoryboardSegue {
         
         
         // Animate the transition.
-        UIView.animateWithDuration(0.4, animations: { () -> Void in
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
             //firstVCView.frame = CGRectOffset(firstVCView.frame, 0.0, -screenHeight)
             secondVCView.frame = CGRectOffset(secondVCView.frame, 0.0, -screenHeight)
             
